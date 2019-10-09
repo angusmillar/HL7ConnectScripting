4bc5fa3f47c5871064fd1a5f25991de5c553d747
angusmillar <angusbmillar@gmail.com>
2019-10-09 18:45:02 +1000

    Removed breakpoint

--- PASFhir/AllergyIntolerance.js
+++ PASFhir/AllergyIntolerance.js
@@ -1,7 +1,7 @@
 function AllergyIntolerance(oAL1) {
 
   this.TypeCode = null;
-  this.Code = null;  
+  this.Code = null;
   this.SeverityCode = null;
   this.ReactionCode = null;
   this.IdentificationDate = null;
@@ -9,7 +9,6 @@ function AllergyIntolerance(oAL1) {
   var oHl7Support = new HL7V2Support();
   if (oAL1 !== null) {
 
-    BreakPoint;
     if (oAL1.Field(2).defined) {
       this.TypeCode = oHl7Support.GetCEByField(oAL1.Field(2))
     }
@@ -17,7 +16,7 @@ function AllergyIntolerance(oAL1) {
     if (oAL1.Field(3).defined) {
       this.Code = oHl7Support.GetCEByField(oAL1.Field(3))
     }
-    
+
     if (oAL1.Field(4).defined) {
       this.SeverityCode = oHl7Support.GetCEByField(oAL1.Field(4))
     }
@@ -34,7 +33,7 @@ function AllergyIntolerance(oAL1) {
         throw "Identification Date & Time in AL1-6 can not be parsed as a Date time, vaule was: " + oAL1.Field(6).AsString;
       }
     }
-    
-  }  
-  
+
+  }
+
 }
